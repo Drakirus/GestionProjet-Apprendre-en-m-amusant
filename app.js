647e580528c6732528eef5192253ca73d9cf01c8
Drakirus <pierrescalade@gmail.com>
2018-01-23 12:10:34 +0100

    up and running for tony

--- app.js
+++ app.js
@@ -9,15 +9,16 @@
   config.$inject = ['$routeProvider', '$locationProvider', 'ChartJsProvider'];
   function config($routeProvider, $locationProvider, ChartJsProvider) {
     $routeProvider
-      .when('/', {
-        controller: 'HomeController',
-        templateUrl: 'home/home.view.html',
-        controllerAs: 'vm'
-      })
 
       .when('/login', {
         controller: 'LoginController',
         templateUrl: 'login/login.view.html',
+        controllerAs: 'vm'
+      })
+
+      .when('/compter', {
+        controller: 'SoloGameController',
+        templateUrl: 'sologame/sologame.view.html',
         controllerAs: 'vm'
       })
 
@@ -48,8 +49,8 @@
 
     $rootScope.$on('$locationChangeStart', function (event, next, current) {
       // redirect to login page if not logged in and trying to access a restricted page
-      var restrictedPage = ['/login', '/register'].indexOf( $location.path() ) === -1;
-      var loggedIn = $rootScope.globals.currentUser;
+      // var restrictedPage = ['/login', '/register'].indexOf( $location.path() ) === -1;
+      // var loggedIn = $rootScope.globals.currentUser;
       // if (restrictedPage && !loggedIn) {
         // $location.path('/login');
       // }
