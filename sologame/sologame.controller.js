ecc85190e0246e38ae68f7ef58caa83058674627
Remi Bouvet <remibouvet72@gmail.com>
2018-01-23 14:59:14 +0100

    correction images et question ecrire

--- sologame/sologame.controller.js
+++ sologame/sologame.controller.js
@@ -39,19 +39,22 @@
 						"url_image": "http://res.cloudinary.com/bdf/image/upload/v1422979850/umyosv2hpo4v6cxdcvvm.jpg",
 						"response": 7,
 						"proposition": [5,4,7,8],
-						"question": "Combien il y a de dinosaures ?"
+						"question": "Combien il y a de dinosaures ?",
+						"text": ""
 					}
 					,{
 						"url_image": "http://res.cloudinary.com/bdf/image/upload/c_fill,h_150,w_260/v1422979850/a7twsbalqszfdqnctw3f.jpg",
 						"response": 5,
 						"proposition": [7,2,3,5],
-						"question": "Combien il y a d'ours en peluche ?"
+						"question": "Combien il y a d'ours en peluche ?",
+						"text": ""
 					},
 					{
 						"url_image": "http://res.cloudinary.com/bdf/image/upload/v1422979850/un9dzblveitmenzb4da0.jpg",
 						"response": 6,
 						"proposition": [7,6,3,5],
-						"question": "Combien il y a de ballon ?"
+						"question": "Combien il y a de ballon ?",
+						"text": ""
 					}
 				],
 				"lire" : [
@@ -59,44 +62,47 @@
 						"url_image": "http://localhost:8080/img/lire1.jpg",
 						"response": "rouge",
 						"proposition": ["rouge","bleu","verte","orange"],
-						"question": "Quelle est la couleur du ballon ?"
+						"question": "Quelle est la couleur du ballon ?",
+						"text": ""
 					}
 					,{
 						"url_image": "http://localhost:8080/img/lire2.jpg",
 						"response": "verte",
 						"proposition": ["rouge","bleu","verte","orange"],
-						"question": "Quelle est la couleur de la pomme ?"
+						"question": "Quelle est la couleur de la pomme ?",
+						"text": ""
 					},
 					{
 						"url_image": "http://localhost:8080/img/lire3.jpg",
 						"response": "bleu",
 						"proposition": ["rouge","bleu","verte","orange"],
-						"question": "Combien il y a de ballon ?"
+						"question": "Combien il y a de ballon ?",
+						"text": ""
 					}
 				],
-	"ecrire" : [
-	{
-	  "url_image": "http://localhost:8080/img/ecrire1.jpg",
-	  "response": "boit",
-	  "proposition": ["boit","mange","joue","dessine"],
-	  "question": "Observe l’image et complète la phrase.",
-		"text": "test"
-	}
-	,{
-	  "url_image": "http://localhost:8080/img/ecrire2.jpg",
-	  "response": "brosse les dents",
-	  "proposition": ["coiffe les cheveux","cache les yeux","brosse les dents"],
-	  "question": "Observe l’image et complète la phrase.",
-		"text": "test"
-	},
-	{
-	  "url_image": "http://localhost:8080/img/ecrire3.jpg",
-	  "response": "peigne",
-	  "proposition": ["cache","plaint","peigne","perd"],
-	  "question": "Observe l’image et complète la phrase.",
-		"text": "test"
-	}
-	]
+				"ecrire" : [
+					{
+						"url_image": "http://localhost:8080/img/ecrire1.jpg",
+						"response": "boit",
+						"proposition": ["boit","mange","joue","dessine"],
+						"question": "Observe l’image et complète la phrase.",
+						"text": "Elle _____ ."
+					}
+					,{
+						"url_image": "http://localhost:8080/img/ecrire2.jpg",
+						"response": "brosse les dents",
+						"proposition": ["coiffe les cheveux","cache les yeux","brosse les dents"],
+						"question": "Observe l’image et complète la phrase.",
+						"text": "Elle se _____ les _____ ."
+					},
+					{
+						"url_image": "http://localhost:8080/img/ecrire3.jpg",
+						"response": "peigne",
+						"proposition": ["cache","plaint","peigne","perd"],
+						"question": "Observe l’image et complète la phrase.",
+						"text": "Il se _____ ."
+					}
+					]
 			}
 			var rand = newGame[gameOption['id']][Math.floor(Math.random() * newGame[gameOption['id']].length)];
 
